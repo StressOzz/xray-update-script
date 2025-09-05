320457105b6adffd641733c4fe6ba0e05866025c
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-09-05 21:23:19 +0500

    Update and rename Xray-Update.sh to Xray-Update0.sh

--- Xray-Update0.sh
+++ Xray-Update0.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
+clear
 set -e
+
+# Цвета
+YELLOW="\033[1;33m"
+GREEN="\033[1;32m"
+RED="\033[1;31m"
+RESET="\033[0m"
 
 CONTAINER="amnezia-xray"
 
-echo "[*] Текущая версия Xray в контейнере:"
-docker exec $CONTAINER xray --version || echo "Xray не найден"
+echo -e "${YELLOW}[*] Текущая версия Xray в контейнере:${RESET}"
+docker exec $CONTAINER xray --version || echo -e "${RED}Xray не найден${RESET}"
 
 # Получаем последнюю версию Xray с GitHub
 LATEST=$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
 
 if [ -z "$LATEST" ]; then
-    echo "[!] Не удалось получить последнюю версию."
+    echo -e "${RED}[!] Не удалось получить последнюю версию.${RESET}"
     exit 1
 fi
 
-echo "[*] Последняя версия: $LATEST"
+echo -e "${YELLOW}[*] Последняя версия: $LATEST${RESET}"
 URL="https://github.com/XTLS/Xray-core/releases/download/v$LATEST/Xray-linux-64.zip"
 
 # Удаляем старый архив и распакованную папку внутри контейнера, если есть
@@ -22,7 +29,7 @@ docker exec $CONTAINER sh -c 'rm -f /tmp/Xray-linux-64.zip'
 docker exec $CONTAINER sh -c 'rm -rf /tmp/xray-new'
 
 # Скачиваем и распаковываем внутри контейнера
-echo "[*] Скачиваем и распаковываем Xray $LATEST внутри контейнера..."
+echo -e "${YELLOW}[*] Скачиваем и распаковываем Xray $LATEST внутри контейнера...${RESET}"
 docker exec $CONTAINER sh -c "
 wget -q -O /tmp/Xray-linux-64.zip $URL &&
 unzip -oq /tmp/Xray-linux-64.zip -d /tmp/xray-new &&
@@ -31,11 +38,11 @@ chmod +x /usr/bin/xray
 "
 
 # Перезапускаем контейнер
-echo "[*] Перезапускаем контейнер..."
+echo -e "${YELLOW}[*] Перезапускаем контейнер...${RESET}"
 docker restart $CONTAINER
 
 # Проверяем версию после обновления
-echo "[*] Новая версия Xray в контейнере:"
+echo -e "${YELLOW}[*] Новая версия Xray в контейнере:${RESET}"
 docker exec $CONTAINER xray --version
 
-echo "[+] Обновление завершено!"
+echo -e "${GREEN}[+] Обновление завершено!${RESET}"
